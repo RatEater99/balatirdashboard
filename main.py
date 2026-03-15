c97f76229eaa87356ca775e0f36b27d7e9374a2e
YuvrajVamp7K <syuvraj2007@gmail.com>
2026-03-15 16:59:18 +0530

    added requirements.txt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import callbacks  # importing registers all callbacks
 
 app.index_string = open("index.html").read()
 app.layout = create_layout()
+server = app.server
 
 if __name__ == "__main__":
     app.run(debug=False)
